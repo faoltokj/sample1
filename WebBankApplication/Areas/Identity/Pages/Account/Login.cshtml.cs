cee0d5f6f9d2dcf1a23743c8eceb9b19a717afdc
Antonio Gaquit <antoniogaquit@gmail.com>
2024-10-06 16:15:38 +0200

    frontend

--- WebBankApplication/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ WebBankApplication/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -115,7 +115,7 @@ namespace WebBankApplication.Areas.Identity.Pages.Account
                 if (result.Succeeded)
                 {
                     _logger.LogInformation("User logged in.");
-                    return LocalRedirect(returnUrl);
+                    return RedirectToPage("/Index");
                 }
                 if (result.RequiresTwoFactor)
                 {
